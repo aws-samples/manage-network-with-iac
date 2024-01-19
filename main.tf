309c2dc130044c1030f8438ca34572e2cb59c772
Pablo Sánchez Carmona <pablosc@amazon.es>
2024-01-19 22:17:26 +0100

    removing stuff from main.tf

--- main.tf
+++ main.tf
@@ -4,32 +4,7 @@
 # --- root/main.tf ---
 
 # ---------- OREGON (us-west-2) ENVIRONMENT ----------
-# Amazon VPCs
-module "oregon_vpcs" {
-  source    = "aws-ia/vpc/aws"
-  version   = "4.4.1"
-  providers = { aws = aws.awsoregon }
 
-  name       = "vpc1"
-  cidr_block = var.vpcs.oregon.vpc1.cidr_block
-  az_count   = var.vpcs.oregon.vpc1.number_azs
-
-  subnets = {
-    workload  = { cidrs = var.vpcs.oregon.vpc1.workload_subnet_cidrs }
-    endpoints = { cidrs = var.vpcs.oregon.vpc1.endpoints_subnet_cidrs }
-  }
-}
-
-# EC2 instances & VPC endpoints
-module "oregon_compute" {
-  source    = "./modules/compute"
-  providers = { aws = aws.awsoregon }
-
-  identifier      = var.identifier
-  vpc_name        = "vpc1"
-  vpc             = module.oregon_vpcs
-  vpc_information = var.vpcs.oregon.vpc1
-}
 
 # ---------- STOCKHOLM (us-west-2) ENVIRONMENT ----------
 
